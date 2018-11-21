be51e7c914aff6f41794839832c939f589c0d1a4
FX Wood <fxwood2@gmail.com>
2018-11-21 01:54:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -31,17 +31,17 @@ function viewCart() {
       return returnString;
       break;
     case 2:
-      for (let i = 0; i < cart.length -1; i++) {
+      for (let i = 0; i < cart.length - 1; i++) {
         returnString += `${cart[i].itemName} at ${cart[i].itemPrice} and`;
       }
-      returnString += `${cart[i].itemName} at ${cart[i].itemPrice}.`
+      returnString += `${cart[cart.length - 1].itemName} at ${cart[cart.length - 1].itemPrice}.`
       return returnString;
       break;
     default:
       for (let i = 0; i < cart.length -1; i++) {
         returnString += `${cart[i].itemName} at ${cart[i].itemPrice} and,`;
       }
-      returnString += `${cart[i].itemName} at ${cart[i].itemPrice}.`
+      returnString += `${cart[cart.length - 1].itemName} at ${cart[cart.length - 1].itemPrice}.`
       return returnString;
   }
 }
