363a09fbb7ae176c33a0e48c357db6d9e65edefc
FX Wood <fxwood2@gmail.com>
2018-11-21 02:36:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -63,7 +63,7 @@ function placeOrder(cardNumber) {
   if (cardNumber) {
     let priceToCharge = total();
     cart = []
-    return `Your total cost is $${priceToCharge}, which will be charged to the card ${cardNumber}`
+    return `Your total cost is $${priceToCharge}, which will be charged to the card ${cardNumber}.`
   } else {
     return "Sorry, we don\'t have a credit card on file for you."
   }
