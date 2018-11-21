16f4aec3c65c6cb704bfbe9ce79471247a96eb77
FX Wood <fxwood2@gmail.com>
2018-11-21 02:27:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ function total() {
 
 function removeFromCart(item) {
   let inCart = false
-  for (i = 0; i < cart.length; i++) {
+  for (let i = 0; i < cart.length; i++) {
     if (item === cart[i].itemName)  {
       cart.splice(i, 1)
       return cart
